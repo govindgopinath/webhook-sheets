0cce88c6f0651ba17eed02bcc7f8be4e03f51627
Govind <35466874+govindgopinath@users.noreply.github.com>
2024-06-04 18:34:53 +0530

    aasdasd

--- merger2.py
+++ merger2.py
@@ -27,8 +27,6 @@ def collect_keys(data, level=0, keys_dict=None, prevkey="", colchanges=None):
     if level>(len(keys_dict)-1):
         if level>0:
             keys_dict.append(['']*len(keys_dict[level-1]))
-        else:
-            keys_dict.append([])
 
     print(keys_dict)
     if isinstance(data, dict):
