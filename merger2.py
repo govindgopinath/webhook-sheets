2b1f38f64b601838f42d675647b116c5303806f9
Govind <35466874+govindgopinath@users.noreply.github.com>
2024-06-18 18:59:45 +0530

    asd

--- merger2.py
+++ merger2.py
@@ -112,8 +112,8 @@ def fill_rows(data, level=0, keys_dict=[],row=[],rowlevel=[0],prevkey=""):
                 elif isinstance(value,list):                                                         
                     for j in range(0,len(value)):
                         if isinstance(value[j],dict):
-                            rowlevel[0] = rowlevel[0]+1
                             fill_rows(value[j],level+1,keys_dict,row,[rowlevel[0]],key)
+                            rowlevel[0] = rowlevel[0]+1
                         else:
                             index = keys_dict[level].index(key)
                             row[rowlevel[0]+j][index] = repr(value)
